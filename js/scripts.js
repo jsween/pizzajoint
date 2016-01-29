8037dea5473cb39beee5830b33f54f1e8c17b9bd
Jon Sweeney <jonsweeney26@gmail.com>
2016-01-28 16:56:35 -0800

    add pizza calc based off size prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ function Pizza(pizzaSize, pizzaToppings) {
   this.pizzaToppings = pizzaToppings;
   this.pizzaPrice = null;
 }
+
+Pizza.prototype.calcPizzaSize = function () {
+  if(this.pizzaSize === "sm") {
+    return this.pizzaPrice += 8;
+  } else if(this.pizzaSize === "md") {
+    return this.pizzaPrice += 12;
+  } else if(this.pizzaSize === "lg") {
+    return this.pizzaPrice += 15;
+  } else
+    return 20;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,19 @@
 describe("Pizza", function() {
   it("will create a new pizza object", function() {
-    var testPizza = new Pizza("small", "pepperoni");
-    expect(testPizza.pizzaSize).to.equal("small");
+    var testPizza = new Pizza("sm", "pepperoni");
+    expect(testPizza.pizzaSize).to.equal("sm");
     expect(testPizza.pizzaToppings).to.equal("pepperoni");
     expect(testPizza.pizzaPrice).to.equal(null);
   });
 
+});
+
+describe("getPizzaSize", function() {
+  it("will return right amount for selected pizza", function() {
+    var testPizza = new Pizza("xl", "pepperoni");
+    // expect(testPizza.calcPizzaSize()).to.equal(8);
+    // expect(testPizza.calcPizzaSize()).to.equal(12);
+    // expect(testPizza.calcPizzaSize()).to.equal(15);
+    expect(testPizza.calcPizzaSize()).to.equal(20);
+  });
 });
