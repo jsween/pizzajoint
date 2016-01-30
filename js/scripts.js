2545f58cdf0d5e6147909c9a952405f9aa4ccb03
Jon Sweeney <jonsweeney26@gmail.com>
2016-01-29 16:06:02 -0800

    clean up code and add comments to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
+/************ Pizza constructor **************/
 function Pizza(pSize, pType, toppings) {
   this.pSize = pSize;
   this.pType = pType;
   this.toppings = toppings;
   this.pizzaPrice = null;
 }
-
+/**** returns num based off pizza size  - used to calc price ****/
 Pizza.prototype.calcPizzaSize = function () {
   if(this.pSize === "Small") {
     return 8;
@@ -15,7 +16,7 @@ Pizza.prototype.calcPizzaSize = function () {
   } else
   return 20;
 };
-
+/**** returns num based off pizza type - used to calc price ****/
 Pizza.prototype.calcPizzaType = function () {
   if(this.pType === "Pepperoni") {
     return 2;
@@ -27,7 +28,7 @@ Pizza.prototype.calcPizzaType = function () {
     return -1;
   };
 };
-
+/**** returns num based off num toppings - used to calc price ****/
 Pizza.prototype.calcPizzaToppings = function () {
   var toppingTotal = 0;
   for(var i = 0; i < this.toppings.length; i++) {
@@ -35,11 +36,11 @@ Pizza.prototype.calcPizzaToppings = function () {
   }
   return toppingTotal;
 };
-
+/**** returns final price of the pizzas ****/
 Pizza.prototype.calcPizzaPrice = function () {
   return this.pizzaPrice = this.calcPizzaSize() + this.calcPizzaType() + this.calcPizzaToppings();
 };
-
+/**** describes the type of pizza ordered ****/
 Pizza.prototype.fullNamePizza = function () {
   return this.pSize + " " + this.pType + " Pizza";
 };
@@ -49,6 +50,7 @@ Pizza.prototype.fullNamePizza = function () {
 $(function() {
   var total = 0;
 
+  /** add a pizza button **/
   $("form#add-pizza").submit(function(event) {
     event.preventDefault();
 
@@ -57,14 +59,24 @@ $(function() {
     var inputtedToppings = $(this).find(".topping:checked").map(function() {
       return this.value;
     });
-
+    /*** makes the toppings into array in order to calc num ***/
     var inputtedToppingsArray = $.makeArray(inputtedToppings);
-
+    /**** new pizza object ****/
     var newPizza = new Pizza(inputtedSize, inputtedType, inputtedToppingsArray);
 
-  total += newPizza.calcPizzaPrice();
-  $("#order-total").text("$ " + total);
-  $("#order-pizzas").append(newPizza.fullNamePizza() + " $" + newPizza.calcPizzaPrice() + "<br>");
+    $("#running-total").show();
+    $(".place-holder").hide();
+    total += newPizza.calcPizzaPrice();
+    $("#order-total").text("$ " + total);
+    $("#order-pizzas").append(newPizza.fullNamePizza() + " $" + newPizza.calcPizzaPrice() + "<br>");
   });
 
+  /** checkout button **/
+  $("#checkout").click(function(event) {
+    if(total > 0) {
+      alert("Your order has been placed!  You owe me $" + total);
+    } else {
+      alert("Hey! Place your order so I can get this going!");
+    }
+  })
 });
